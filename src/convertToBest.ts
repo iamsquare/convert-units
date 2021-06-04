73b1ba49779358c605f430273a052a5444c6fe97
Marco Cesi <sq@iamsquare.it>
2021-06-05 01:34:21 +0200

    Add convert to best documentation

--- src/convertToBest.ts
+++ src/convertToBest.ts
@@ -1,5 +1,4 @@
-import { contains, curry, filter, ifElse, pipe, reduce } from 'ramda';
-import { lengthGt } from 'ramda-adjunct';
+import { contains, curry, filter, pipe, reduce } from 'ramda';
 
 import convert from './convert';
 import { UnitType } from './definitions';
@@ -8,6 +7,24 @@ import list from './list';
 import { BestConversion, ConvertToBestDto, Measure, UnitDescription } from './type';
 import { Nullable } from './type/utils.type';
 
+/**
+ * Converts a `value` from a compatible unit to the best unit (or _smallest_) given some optional parameters.
+ *
+ * This function is **curried**.
+ * @see {@link https://ramdajs.com/docs/#curry | Ramda's documentation} to understand how curried functions work.
+ *
+ * @throws An Error if `from` is not a valid {@link UnitType}
+ *
+ * @param options The conversion configuration
+ * @param from The type `from` which you want to convert
+ * @param value The value you want to convert
+ * @returns An object containing the best unit given the optional parameters and its converted value
+ *
+ * ```typescript
+ * const convertToBestFromGrams = convertToBest({}, 'g');
+ * convertToBestFromGrams(1000); // => { value: 1, unitType: 'kg' }
+ * ```
+ */
 const convertToBest = curry((options: Partial<ConvertToBestDto>, from: UnitType, value: number) => {
   const opt: ConvertToBestDto = { cutoff: 1, exclude: [], ...options };
 
