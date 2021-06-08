61e847c77ec88b01ea4572b9dbf5f84db5a591b5
Marco Cesi <sq@iamsquare.it>
2021-06-08 17:56:25 +0200

    Add some translation key types Force, frequency, illuminance, mass, pace

---
+++ src/i18n/type/illuminance.ts
@@ -0,0 +1,10 @@
+export enum IlluminanceTranslationEnum {
+  SINGULAR_LUX = 'SINGULAR_LUX',
+  SINGULAR_FOOT_CANDLE = 'SINGULAR_FOOT_CANDLE',
+  PLURAL_LUX = 'PLURAL_LUX',
+  PLURAL_FOOT_CANDLE = 'PLURAL_FOOT_CANDLE'
+}
+
+export type IlluminanceSingularTranslationKey = 'SINGULAR_LUX' | 'SINGULAR_FOOT_CANDLE';
+
+export type IlluminancePluralTranslationKey = 'PLURAL_LUX' | 'PLURAL_FOOT_CANDLE';
