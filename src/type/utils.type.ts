801a1f88ddcc990bf3a00320d7c4601f3c3fdfdd
Marco Cesi <sq@iamsquare.it>
2021-06-12 03:14:01 +0200

    Add some translations

--- src/type/utils.type.ts
+++ src/type/utils.type.ts
@@ -3,7 +3,7 @@ export type Maybe<T> = T | undefined;
 export type CamelToPascalCase<S extends string> = S extends `${infer T}${infer U}`
   ? `${T extends Capitalize<T> ? '_' : ''}${Uppercase<T>}${CamelToPascalCase<U>}`
   : S;
-export type PartialRecord<T extends keyof any, U> = { [P in T]?: U };
+export type PartialRecord<T extends keyof any, U> = Partial<Record<T, U>>;
 export type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<keyof T, Keys>> &
   {
     [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>;
